2e6d720bf237c66363f5a6dda591eda92eada4a6
SarahKinner <sarahkinner0@gmail.com>
2025-10-04 03:12:44 -0400

    Do the code of what it will save in the program if the passage being used is using one verse only

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -4,4 +4,13 @@ pubilc class Reference
     private int _chapter;
     private int _verseStart;
     private int _verseEnd;
+
+    //Base if the passage if it is a single verse
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verse;
+        _verseEnd = null;
+    }
 }
