db26dbd3131dbdbe495721ee19e63ee3b7232cbc
SarahKinner <sarahkinner0@gmail.com>
2025-10-04 12:04:04 -0400

    Created a library to hold multiple scripture passages for the creativity part of the project

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,11 +5,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        string text = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. " +
-                      "In all thy ways acknowledge him, and he shall direct thy paths.";
-        
-        Scripture scripture = new Scripture(reference, text);
+        List<Scripture> scriptureLibrary = new List<Scripture>();
+
+        scriptureLibrary.Add(new Scripture(
+            new Reference("Proverbs", 3, 5, 6),
+            "Trust in the Lord with all thine heart; and lean not unto thine own understanding. " +
+            "In all thy ways acknowledge him, and he shall direct thy paths."
+        ));
+
+        scriptureLibrary.Add(new Scripture(
+            new Reference("John", 3, 16),
+            "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+        ));
+
+        scriptureLibrary.Add(new Scripture(
+            new Reference("Mosiah", 2, 17),
+            "When ye are in the service of your fellow beings ye are only in the service of your God."
+        ));
+
+        scriptureLibrary.Add(new Scripture(
+            new Reference("Alma", 37, 6),
+            "By small and simple things are great things brought to pass; and small means in many instances doth confound the wise."
+        ));
 
         while (!scripture.IsCompletelyHidden())
         {
