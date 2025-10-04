6cbbe9bf32ba7c7bb9359c47cfc934b9ceeec87f
SarahKinner <sarahkinner0@gmail.com>
2025-10-04 09:30:49 -0400

    Added a scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        string text = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. " +
+                      "In all thy ways acknowledge him, and he shall direct thy paths.";
+        
+        Scripture scripture = new Scripture(reference, text);
     }
 }
