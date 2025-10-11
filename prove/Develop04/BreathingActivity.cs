7b67c4d9e1842cb512dc0886f82a583153719d90
SarahKinner <sarahkinner0@gmail.com>
2025-10-11 11:04:57 -0400

    Code how the activity will run and how long each of will go until the next instruction is given

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -5,4 +5,17 @@ public class BreathingActivity : Activity
 {
     public BreathingActivity()
         : base("Breathing", "This activity helps you relax by guiding your breathing.") { }
+
+    protected override void RunActivity()
+    {
+        DateTime end = DateTime.Now.AddSeconds(_duration);
+        while (DateTime.Now < end)
+        {
+            Console.Write("Breathe in...");
+            PauseCountdown(4);
+            Console.Write("Breathe out...");
+            PauseCountdown(4);
+            Console.WriteLine();
+        }
+    }
 }
