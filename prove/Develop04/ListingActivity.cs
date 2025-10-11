87b73d26c3eb087252b9e7181bd629beaaa08874
SarahKinner <sarahkinner0@gmail.com>
2025-10-11 11:22:50 -0400

    Code so it can randomly pick a prompt have a timer for the activity and shows off how much was listed by the user

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -12,4 +12,23 @@ public class ListingActivity : Activity
 
     public ListingActivity()
         : base("Listing", "This activity helps you list positive things to lift your mood") { }
+
+    protected override void RunActivity()
+    {
+        Random rand = new Random();
+        string prompt = _prompts[rand.Next(_prompts.Length)];
+        Console.WriteLine($"\n{prompt}");
+        Console.WriteLine("Start listing items. You have a few seconds!");
+        PauseCountdown(3);
+
+        DateTime end = DateTime.Now.AddSeconds(_duration);
+        List<string> responses = new();
+        while (DateTime.Now < end)
+        {
+            Console.Write("> ");
+            responses.Add(Console.ReadLine());
+        }
+
+        Console.WriteLine($"\nYou listed {responses.Count} items!")
+    }
 }
