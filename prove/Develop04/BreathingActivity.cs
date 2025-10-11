3954c035610a3ea8678cd88a4edc1d94ab7fd177
SarahKinner <sarahkinner0@gmail.com>
2025-10-11 11:01:23 -0400

    Shows the descriptiong for the Breathing Activity

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,8 @@
+using  System;
+using System.Threading;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity()
+        : base("Breathing", "This activity helps you relax by guiding your breathing.") { }
+}
