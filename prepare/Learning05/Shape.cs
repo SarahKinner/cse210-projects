8c6e0b82cdfffe26a370f887c3a7a18a355e3688
SarahKinner <sarahkinner0@gmail.com>
2025-10-13 17:08:43 -0400

    Program for the prepare assignment yaaaaaaayyyyy

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+using System;
+
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    // Abstract method that each shape will override
+    public abstract double GetArea();
+}
