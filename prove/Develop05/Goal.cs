520458b9afd4c5028bd5b7e307594140c2292acc
SarahKinner <sarahkinner0@gmail.com>
2025-10-16 13:36:11 -0400

    well actually this shows the user the title, description, and points the last one just keeps it protected from other classes

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -5,4 +5,11 @@ public abstract class GoalManager
     protected string _title;
     protected string _description;
     protected int _points;
+
+    public Goal(string title, string _description, int points)
+    {
+        _title = title;
+        _description = description;
+        _points = points;
+    }
 }
