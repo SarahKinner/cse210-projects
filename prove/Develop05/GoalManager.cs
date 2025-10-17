c3bd8fdba41decfdb51fb29f35451a397ba4509b
SarahKinner <sarahkinner0@gmail.com>
2025-10-17 17:43:39 -0400

    Made the menu

---
+++ prove/Develop05/GoalManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class GoalManager
+{
+    private List<Goal> _goals = new List<Goal>();
+    private int _score = 0;
+
+    public void Start()
+    {
+        string choice = "";
+        while (choice != "6")
+        {
+            Console.WriteLine($"\nYou have {_score} points.\n");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.Write("Select a choice: ");
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    CreateGoal();
+                    break;
+                case "2":
+                    ListGoals();
+                    break;
+                case "3":
+                    SaveGoals();
+                    break;
+                case "4":
+                    LoadGoals();
+                    break;
+                case "5":
+                    RecordEvent();
+                    break;
+            }
+        }
+    }
+}
