9bc530d64a77de61474f0563bfc0c3084f1c0468
SarahKinner <sarahkinner0@gmail.com>
2025-10-17 13:12:42 -0400

    Create a goal with title, description, points, target, and bonus points, along with starting at 0

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -5,4 +5,12 @@ public class EternalGoal : Goal
     private int _amountCompleted;
     private int _targetAmount;
     private int _bonus;
+
+    public EternalGoal(string title, string description, int points, int targetAmount, int bonus)
+        : base(title, description, points)
+    {
+        _amountCompleted = 0;
+        _targetAmount = targetAmount;
+        _bonus = bonus;
+    }
 }
