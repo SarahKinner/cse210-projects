57e28caf1d761e7efd86b0f543cd8f6d4b470a0d
SarahKinner <sarahkinner0@gmail.com>
2025-10-17 13:20:40 -0400

    Code what the program does if the goal gets reached

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -13,4 +13,14 @@ public class EternalGoal : Goal
         _targetAmount = targetAmount;
         _bonus = bonus;
     }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+        Console.WriteLine($"You completed '{_title}' {_amountCompleted}/{_targetAmount} times and earned {_points} points!");
+        if (_amountCompleted == _targetAmount)
+        {
+            Console.WriteLine($"🎉 Bonus achieved! You earned an extra {_bonus} points!");
+        }
+    }
 }
