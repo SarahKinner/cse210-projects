29d1ac965c51a68515c3472ac8f5328e82d55820
SarahKinner <sarahkinner0@gmail.com>
2025-10-23 16:04:02 -0400

    Made the Comment class

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,10 @@
-using System;
+class Comment:
+    def __init__(self, commenter_name, comment_text):
+        self.commenter_name = commenter_name
+        self.comment_text = comment_text
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
-    }
-}
+    def get_commenter_name(self):
+        return self.commenter_name
+
+    def get_comment_text(self):
+        return self.comment_text
