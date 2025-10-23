439540279b1e21d3eec88b796d8e22847a7e1fba
SarahKinner <sarahkinner0@gmail.com>
2025-10-23 16:17:45 -0400

    Coded the Video class

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,3 +8,30 @@ class Comment:
 
     def get_comment_text(self):
         return self.comment_text
+
+class Video:
+    def __init__(self, title, author, length_in_seconds):
+        self.title = title
+        self.author = author
+        self.length_in_seconds = length_in_seconds
+        self.comments = []
+
+    def add_comment(self, comment):
+        """Add a Comment object to the video"""
+        self.comments.append(comment)
+
+    def get_number_of_comments(self):
+        """Return how many comments are on this video"""
+        return len(self.comments)
+
+    def display_video_info(self):
+        """Display basic video details"""
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Length: {self.length_in_seconds} seconds")
+        print(f"Number of Comments: {self.get_number_of_comments()}")
+        print("Comments:")
+
+        for comment in self.comments:
+            print(f"- {comment.get_commenter_name()}: {comment.get_comment_text()}")
+        print("-" * 40)
