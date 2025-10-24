965b9f6bf77b01c0631bdaae5c16ddbe832079b0
SarahKinner <sarahkinner0@gmail.com>
2025-10-24 13:05:55 -0400

    Added customer info

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -7,5 +7,8 @@ class Program
     {
         Address address1 = new Address("123 Maple St", "Rexburg", "ID", "USA")
         Address address2 = new Address("45 River Road", "Toronto", "ON", "Canada");
+
+        Customer customer1 = new Customer("Sarah Kinner", address1);
+        Customer customer2 = new Customer("Marie Brown", address2);
     }
 }
