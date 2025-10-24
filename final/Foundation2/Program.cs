e477249df2275d641b4be2ae0f46907ce6b93f65
SarahKinner <sarahkinner0@gmail.com>
2025-10-24 13:16:49 -0400

    Created the orders

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -10,5 +10,14 @@ class Program
 
         Customer customer1 = new Customer("Sarah Kinner", address1);
         Customer customer2 = new Customer("Marie Brown", address2);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop", "A123", 899.99, 1));
+        order1.AddProduct(new Product("Mouse", "B456", 25.50, 2));
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Headphones", "C789", 120.00, 1));
+        order2.AddProduct(new Product("Keyboard", "D321", 75.99, 1));
+        order2.AddProduct(new Product("Monitor", "E654", 199.99, 1));
     }
 }
