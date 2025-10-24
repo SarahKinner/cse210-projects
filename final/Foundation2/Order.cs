7e1f5fbfc8b5d94f42542f033163fa06636e7649
SarahKinner <sarahkinner0@gmail.com>
2025-10-24 11:19:04 -0400

    Made code that it holds info about the products for the order, the cusomter making it, and creates a new order when needed to

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+        _products = new List<Product>();
+    }
+}
