f5c1426ef4ed185711c0f2d1154a5af2f862e20b
SarahKinner <sarahkinner0@gmail.com>
2025-10-24 10:57:17 -0400

    Stores the info for name and address

--- final/Foundation2/Customer.cs
+++ final/Foundation2/Customer.cs
@@ -4,4 +4,10 @@ public class Customer
 {
     private string _name;
     private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
 }
