ff60669773cc362bcae6d8a47234ff5442138e83
SarahKinner <sarahkinner0@gmail.com>
2025-10-24 12:49:34 -0400

    Made addresses

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address address1 = new Address("123 Maple St", "Rexburg", "ID", "USA")
+        Address address2 = new Address("45 River Road", "Toronto", "ON", "Canada");
     }
 }
