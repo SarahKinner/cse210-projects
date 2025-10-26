f1c66809a8fa6d17c36e407009df89f186538d60
SarahKinner <sarahkinner0@gmail.com>
2025-10-26 11:34:11 -0400

    Added code to Reception.cs so it stores the email address and can create a new Reception object if needed

---
+++ final/Foundation3/Reception.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Reception : Event
+{
+    private string _rsvpEmail;
+
+    pubilc Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+        : base(title, description, date, time, address)
+    {
+        _rsvpEmail = rsvpEmail;
+    }
+}
