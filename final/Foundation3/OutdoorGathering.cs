cc3a83a05d4b75de127fccdd69e64f003d6483b3
SarahKinner <sarahkinner0@gmail.com>
2025-10-26 14:17:05 -0400

    Added code to OutdoorGathering.cs to store info and adds object if needed

---
+++ final/Foundation3/OutdoorGathering.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class OutdoorGathering : Event
+{
+    private string _weatherForecast;
+
+    public OutdoorGathering(string title, string description, string date, string time, Address address, string weatherForecast)
+        : base(title, description, date, time, address)
+    {
+        _weatherForecast = weatherForecast
+    }
+}
