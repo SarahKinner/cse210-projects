cffcd0ffd735d7f6d5be497b7f613bb72d553d0d
SarahKinner <sarahkinner0@gmail.com>
2025-10-26 17:19:35 -0400

    Created addresses in Program.cs

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address1 = new Address("123 Main St", "Prvo", "UT", "USA");
+        Address address2 = new Address("45 Ocean Blvd", "Miami", "FL", "USA");
+        Address address3 = new Address("99 Pine Way", "Seattle", "WA", "USA");
     }
 }
