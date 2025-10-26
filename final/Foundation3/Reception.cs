c2aa7ab43b1033c88f1fa0cbc194a112b367df60
SarahKinner <sarahkinner0@gmail.com>
2025-10-26 11:42:05 -0400

    Added code to Reception.cs for it to put its own unique spin to the Event code and gives info on the reception

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -4,9 +4,19 @@ public class Reception : Event
 {
     private string _rsvpEmail;
 
-    pubilc Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+    public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
         : base(title, description, date, time, address)
     {
         _rsvpEmail = rsvpEmail;
+    }
+
+    public override string GetFullDetails()
+    {
+        return $"{GetStandardDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
+    }
+
+    public override string GetShortDescription()
+    {
+        return $"Reception - RSVP: {_rsvpEmail}";
     }
 }
