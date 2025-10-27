8f3c87231cd49fd53775e79cde9ec410d2df7dae
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 08:59:13 -0400

    Added code to Swimming.cs so it stores number of laps, create new swimming object, sends info to Parent class, and stores laps for the session

---
+++ final/Foundation4/Swimming.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+}
