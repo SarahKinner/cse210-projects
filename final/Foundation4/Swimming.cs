86f1b14cb96cb399d5b171c392a712e97369d252
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 09:12:38 -0400

    Added code to Swimming.cs to calculate total distance, speed, minutes per km, and gives out a summary easy for the user to read

--- final/Foundation4/Swimming.cs
+++ final/Foundation4/Swimming.cs
@@ -9,4 +9,26 @@ public class Swimming : Activity
     {
         _laps = laps;
     }
+
+    public override double GetDistance()
+    {
+        double distanceKm = _laps * 50 / 1000.0;
+        return distanceKm;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate()} Swimming ({GetMinutes()} min) - " +
+               $"Distance: {GetDistance():F2} km, Speed: {GetSpeed():F2} kph, Pace: {GetPace():F2} min per km";
+    }
 }
