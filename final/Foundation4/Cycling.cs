766d31727b6c63d2b2e4cf96ff621fa1b842ee9e
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 08:53:08 -0400

    Added code to Cycling.cs so it can calculate total distance traveled, time per mile, and makes a summary easy for the user to read

--- final/Foundation4/Cycling.cs
+++ final/Foundation4/Cycling.cs
@@ -14,4 +14,20 @@ public class Cycling : Activity
     {
         return _speed;
     }
+
+    public override double GetDistance()
+    {
+        return (_speed * GetMinutes()) / 60;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate()} Cycling ({GetMinutes} min) - " +
+               $"Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+    }
 }
