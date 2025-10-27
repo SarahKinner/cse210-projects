67f346f50997b805b852c18231dc652ce6666ae4
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 19:52:11 -0400

    Added code in Foundation 4 Activity.cs to calculate calories made it more near for creativity and convience

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,6 +1,6 @@
 using System;
 
-public class Activity
+public abstract class Activity
 {
     private string _date;
     private int _minutes;
@@ -11,33 +11,18 @@ public class Activity
         _minutes = minutes;
     }
 
-    public string GetDate()
-    {
-        return _date;
-    }
+    public string GetDate() => _date;
+    public int GetMinutes() => _minutes;
 
-    public int GetMinutes()
-    {
-        return _minutes;
-    }
-
-    public virtual double GetDistance()
-    {
-        return 0;
-    }
-
-    public virtual double GetSpeed()
-    {
-        return 0;
-    }
-
-    public virtual double GetPace()
-    {
-        return 0;
-    }
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+    public abstract double GetCalories();
 
     public virtual string GetSummary()
     {
-        return $"{_date} Activity ({_minutes} min)";
+        return $"{_date} {this.GetType().Name} ({_minutes} min) - " +
+               $"Distance: {GetDistance():F2} miles, Speed: {GetSpeed():F2} mph, Pace: {GetPace():F2} min/mile, " +
+               $"Calories: {GetCalories():F0} kcal";
     }
 }
