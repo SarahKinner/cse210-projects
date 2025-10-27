8b74f625e111eeb1bd06e0e92beee0fd54f519e0
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 08:09:41 -0400

    Created the files needed for Foundation 4 and added code to Activity.cs to store and have access to date and duration

---
+++ final/Foundation4/Activity.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+}
