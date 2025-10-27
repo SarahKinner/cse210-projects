3c9a041b6c328f3e5b25ffa0511e71aec26d9bbe
SarahKinner <sarahkinner0@gmail.com>
2025-10-27 08:23:24 -0400

    Added code to Activity.cs to return default values and can be overriden to what the activity needs it to be

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -20,4 +20,24 @@ public class Activity
     {
         return _minutes;
     }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual stating GetSummary()
+    {
+        return $"{date} Activity ({_minutes} min)";
+    }
 }
